d3c3863005fa38cd1f2690b02f256bde62eb610b
Karima-m <karima.02@outlook.dk>
2024-05-03 23:16:59 +0200

    Updated code

---
+++ features/01_process_images.py
@@ -0,0 +1,76 @@
+from extract_features import asymmetry, color, dg
+import matplotlib.pyplot as plt
+import os
+
+def load(image_dir, mask_dir):
+
+    """
+    Function to load images and masks
+
+    Inputs:
+     - Directory for images
+     - Directory for masks
+
+    Output:
+     - List of images
+     - List of masks
+     - List of filenames/image ID's
+    """
+
+    # Initialize lists to store images and masks
+    images = []
+    masks = []
+    filenames = []
+
+    # Iterate over image files
+    for im_filename in os.listdir(image_dir):
+
+        if im_filename.endswith('.png'):  
+            
+            # Load image
+            image_path = os.path.join(image_dir, im_filename)
+
+            image = plt.imread(image_path)
+            images.append(image)
+
+            # Load corresponding mask
+            mask_filename = os.path.splitext(im_filename)[0] + '_mask.png' 
+            mask_path = os.path.join(mask_dir, mask_filename)
+
+            mask = plt.imread(mask_path)
+            masks.append(mask)
+
+            filename = im_filename.rstrip('.png')
+            filenames.append(filename)
+
+    return images, masks, filenames
+
+# Directory paths for images and masks
+image_dir = input("Please enter image directory: ")
+mask_dir = input("Please enter mask directory: ")
+output_folder = input("Please enter output folder path: ")
+
+# Load images and masks
+images, masks, filenames = load(image_dir, mask_dir)
+
+# Path to the output CSV file
+output_path = os.path.join(output_folder, "features.csv")
+
+# CSV file with the results
+# CSV file with the results
+with open(output_path, "w") as outfile:
+
+    outfile.write(f"image_id,A,C,DG\n")
+
+    for index, (im, mask) in enumerate(zip(images, masks)):
+        
+        im_id = filenames[index]
+
+        # Call functions
+        a_score = asymmetry(im, mask)
+        c_score = color(im, mask)
+        dg_score = dg(im, mask)
+
+        outfile.write(f"{im_id},{a_score},{c_score},{dg_score}\n")
+
+print('A CSV file has been created with the results!')
---
+++ features/02_train_classifiers.py
@@ -0,0 +1,100 @@
+import pandas as pd
+import numpy as np
+import pickle
+
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split, StratifiedKFold
+from sklearn.metrics import roc_auc_score, confusion_matrix
+
+
+label_data = "/Users/karimamahdaoui/Documents/GitHub/pds2024/data/ground_truth.csv"
+feature_data = "/Users/karimamahdaoui/Documents/GitHub/pds2024/data/features.csv"
+
+df_labels = pd.read_csv(feature_data)
+df_feat= pd.read_csv(label_data)
+
+df = pd.merge(df_labels, df_feat, left_on=["image_id"], right_on=["image_id"])[["melanoma","A","C", "DG"]]
+
+X = df[list(df.columns)[1:]]
+y = df["melanoma"]
+
+
+# Splitting the data into train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+
+
+# Prepare cross-validation
+num_folds = 5
+skf = StratifiedKFold(n_splits=num_folds)
+
+#Different classifiers to test out
+classifiers = [
+    KNeighborsClassifier(n_neighbors=1),
+    KNeighborsClassifier(n_neighbors=3),
+    KNeighborsClassifier(n_neighbors=5)
+]
+num_classifiers = len(classifiers)
+
+AUC_val = np.empty([num_folds,num_classifiers])
+
+true_labels = [[] for _ in range(num_classifiers)]
+predicted_labels = [[] for _ in range(num_classifiers)]
+
+# Loop through the folds
+for i, (train_index, test_index) in enumerate(skf.split(X_train, y_train)):
+    
+    # Extract the train and test data for this fold
+    x_train_fold, x_val_fold = X_train.iloc[train_index], X_train.iloc[test_index]
+    y_train_fold, y_val_fold = y_train.iloc[train_index], y_train.iloc[test_index]
+
+    for j, clf in enumerate(classifiers): 
+        
+        # Fit the classifier on the training data
+        clf.fit(x_train_fold, y_train_fold)
+
+        # Predict the labels for the validation set
+        pred = clf.predict(x_val_fold)
+    
+        #Evaluate your metric of choice (accuracy is probably not the best choice)
+        AUC_val[i,j] = roc_auc_score(y_val_fold, pred)
+
+        # Append true labels and predicted labels for this fold
+        true_labels[j].extend(y_val_fold)
+        predicted_labels[j].extend(pred)
+
+#Average over all folds
+average_acc = np.mean(AUC_val, axis=0) 
+
+print('Classifier 1 average accuracy={:.3f} '.format(average_acc[0]))
+print('Classifier 2 average accuracy={:.3f} '.format(average_acc[1]))
+
+
+# Loop through each classifier
+for j, clf in enumerate(classifiers):
+    
+    # Train the classifier on the entire training set
+    clf.fit(X_train[['A', 'C', 'DG']], y_train)
+
+    # Make predictions on the test set
+    y_pred_test = clf.predict_proba(X_test[['A', 'C', 'DG']])[:, 1]  # Probability of class 1
+
+    # Calculate AUC score on test set
+    auc_score_test = roc_auc_score(y_test, y_pred_test)
+    print(f"\nAUC Score for classifier {j+1} on test set: {auc_score_test}")
+
+    # Compute confusion matrix
+    conf_matrix = confusion_matrix(true_labels[j], predicted_labels[j])
+    print("\nConfusion Matrix:")
+    print(conf_matrix)
+
+
+# Final classifier
+#classifier = KNeighborsClassifier(n_neighbors = 5)
+
+# Training this classifier on the entire dataset
+#classifier = classifier.fit(X,y)
+
+# Saving classifier
+#filename = 'groupK_classifier.sav'
+#pickle.dump(classifier, open(filename, 'wb'))
